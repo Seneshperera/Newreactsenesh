7a38fa962b436940cacc53c83c82aae74beda3f0
Seneshperera <seneshperera2000@gmail.com>
2024-12-31 01:48:10 +0530

    Update Pagetwo.jsx file with layout adjustments

--- src/components/Pagetwo.jsx
+++ src/components/Pagetwo.jsx
@@ -1,38 +1,85 @@
-import React from 'react'
+import React from "react";
 
-function pagetwo() {
+function PageTwo() {
   return (
-    <div className='h-screen bg-cover bg-center bg-[url("public\project1\Gradient bg.png")]'>
+    <div
+      className="flex flex-col items-center min-h-screen py-10"
+      style={{
+        backgroundImage: "url('public/project1/Gradient bg.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      
+      <div className="flex flex-row w-full justify-between px-10 mt-10">
+        
+        <div className="w-1/3 text-left space-y-4 relative">
+          <h1 className="text-gray-400 text-2xl">01</h1>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
+            <br /> Faucibus in libero risus semper habitant arcu eget.
+          </p>
 
-        <div className='text-left ml-10'>
-            <h1 className='text-gray-400'>01</h1>
-            <p className='text-gray-700'>Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit.<br/> Faucibus in libero
-            risus semper habitant arcu eget.</p>
+          <div className="mt-5">
+            <img
+              src="public/project1/Explosion.png"
+              alt="Explosion"
+              className=""
+            />
+          </div>
+          <div>
+          <img
+            src="public/project1/Image bottom2.png"
+            alt="Bottom Image"
+            className="mt-6"
+          />
+          </div>
+          
         </div>
 
-      <div>
-        <h1 className='text-6xl text-center font-semibold mt-30'>Create <br/> Your <br/> Deisgn</h1>
-        <p className="text-gray-600 font-thin text-center text-xl mt-6">
-          Lorem ipsum dolor sit <br/> amet, consectetur <br/> adipiscing elit. Faucibus <br/> in libero
-          risus semper <br/> habitant arcu eget.
-        </p>
-        <div className='flex justify-center '>
-        <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-10 w-28 items-center">
+        
+        <div className="text-center mx-auto">
+          <h1 className="text-6xl font-semibold leading-tight">
+            Create <br /> Your <br /> Design
+          </h1>
+          <p className="text-gray-600 font-thin text-xl mt-6 leading-relaxed">
+            Lorem ipsum dolor sit <br /> amet, consectetur <br /> adipiscing
+            elit. Faucibus <br /> in libero risus semper <br /> habitant arcu
+            eget.
+          </p>
+          <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-20 w-28">
           Explore
         </button>
         </div>
 
-        <div className='flex justify-end mr-10 pb-5'>
-        <div className='text-left justify-end'>
-            <h1 className='text-gray-400'>02</h1>
-            <p className='text-gray-700'>Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit. <br/> Faucibus in libero
-            risus semper habitant arcu eget.</p>
-        </div>
-        </div>
         
+        <div className="relative w-1/3">
+          <div className="bg-blue-300 rounded-full w-48 h-48 absolute right-10 top-5"></div>
+          <img
+            src="public/project1/Image top2.png"
+            alt="Top Image"
+            className="relative"
+          />
+        
+          <div className="mt-5">
+            <img
+              src="public\project1\Party.png"
+              alt="party"
+              className=""
+            />
+          </div>
+
+          <div className="text-left space-y-4">
+          <h1 className="text-gray-400 text-2xl">02</h1>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.{" "}
+            <br /> Faucibus in libero risus semper habitant arcu eget.
+          </p>
+        </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default pagetwo
+export default PageTwo;

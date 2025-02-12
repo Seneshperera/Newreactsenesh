d74d406898237e3dd50b4ed3bc2579c7ee9432fe
Seneshperera <seneshperera2000@gmail.com>
2025-02-12 19:16:03 +0530

    Initial commit

--- src/components/Pagetwo.jsx
+++ src/components/Pagetwo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PageTwo() {
   return (
     <div
-      className="flex flex-col items-center min-h-screen py-10"
+      className="flex flex-col items-center min-h-screen py-10 px-5 md:px-10"
       style={{
         backgroundImage: "url('public/project1/Gradient bg.png')",
         backgroundSize: "cover",
@@ -11,9 +11,9 @@ function PageTwo() {
       }}
     >
       
-      <div className="flex flex-row w-full justify-between px-10 mt-10">
+      <div className="flex flex-col md:flex-row w-full justify-between mt-10 space-y-10 md:space-y-0">
         
-        <div className="w-1/3 text-left space-y-4 relative">
+        <div className="w-full md:w-1/3 text-left space-y-4 relative">
           <h1 className="text-gray-400 text-2xl">01</h1>
           <p className="text-gray-700">
             Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
@@ -42,18 +42,18 @@ function PageTwo() {
           <h1 className="text-6xl font-semibold leading-tight">
             Create <br /> Your <br /> Design
           </h1>
-          <p className="text-gray-600 font-thin text-xl mt-6 leading-relaxed">
+          <p className="text-gray-600 font-thin text-lg md:text-xl mt-6 leading-relaxed">
             Lorem ipsum dolor sit <br /> amet, consectetur <br /> adipiscing
             elit. Faucibus <br /> in libero risus semper <br /> habitant arcu
             eget.
           </p>
-          <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-20 w-28">
+          <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-10 md:mt-20 w-28">
           Explore
         </button>
         </div>
 
         
-        <div className="relative w-1/3">
+        <div className="relative w-full md:w-1/3">
           <div className="bg-blue-300 rounded-full w-48 h-48 absolute right-10 top-5"></div>
           <img
             src="public/project1/Image top2.png"

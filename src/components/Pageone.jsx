74eef9fccc567908d7f0c5c25ca68af360413abc
Seneshperera <seneshperera2000@gmail.com>
2025-02-14 19:00:40 +0530

    Added new files

--- src/components/Pageone.jsx
+++ src/components/Pageone.jsx
@@ -3,56 +3,54 @@ import React from 'react';
 function Pageone() {
   return (
     <div className="flex flex-col md:flex-row p-7 gap-5">
-       
       {/* Left Content */}
       <div className="text-left md:w-1/2">
         <h1 className="text-6xl font-normal mb-5">
-          Discover.<br />Explore,<br />Inspire.
+          Discover.<br />Explore.<br />Inspire.
         </h1>
-        <p className="text-gray-600 mb-7 h-10 w-[416px] leading-relaxed text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero
-          risus semper habitant arcu eget.
+        <p className="text-gray-600 mb-7 w-[416px] leading-relaxed text-xl">
+        Lorem ipsum dolor sit amet,consectetur <br/> adipiscing elit. Faucibus in libero risus <br/>
+        semper habitant arcu eget. Et integer<br/> facilisi eget diam.
         </p>
-        <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-20 w-28">
+        <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition  w-28">
           Explore
         </button>
 
         <img
-        className='mt-8 ml-20 h-48' 
-        src='./project1/Spiral 3.png'
-        alt='Sprial3'
+          className='mt-8 ml-20 h-48' 
+          src='./project1/Spiral 3.png'
+          alt='Spiral3'
         />
-
       </div>
 
-      <div>
-            <img 
-            className='mt-8 items-center h-20 w-72 '
-            src='./project1/shape wave.png'
-            alt='shape wave'
-            />
-        </div>
-
-      {/* Right Content */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:w-4/5">
-        <img
-          className="h-72 w-96 mt-48 object-cover rounded-lg  "
-          src="public\project1\image3.jfif"
-          alt="First Image"
+      {/* Middle Content */}
+      <div className="flex flex-col items-center">
+        <img 
+          className='h-20 w-72'
+          src='./project1/shape wave.png'
+          alt='shape wave'
         />
         <img
-          className="h-72 w-96 mt-24 object-cover rounded-lg "
-          src="public\project1\image2.jfif"
+          className="h-80 w-96 mt-48 object-cover rounded-lg hover:shadow-lg hover:shadow-gray-500 transition-shadow"
+          src="public/project1/image3.jfif"
+          alt="First Image"
+        />
+      </div>
+
+      {/* Right Content */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 md:w-4/5">
+        <img
+          className="h-80 w-96 mt-24 object-cover rounded-lg hover:shadow-lg hover:shadow-gray-500 transition-shadow"
+          src="public/project1/image2.jfif"
           alt="Second Image"
         />
         <img
-          className="h-72 w-96 mt-o object-cover rounded-lg "
-          src="public\project1\image1.jfif"
+          className="h-80 w-96 mt-0 object-cover rounded-lg hover:shadow-lg hover:shadow-gray-500 transition-shadow"
+          src="public/project1/image1.jfif"
           alt="Third Image"
         />
-        
-        <p className='ml-64  w-80 h-6 text-gray-600'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam.
+        <p className='ml-64 w-80 h-6 text-gray-600'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget. Et integer facilisi eget diam.
         </p>
       </div>
     </div>
--- src/components/Pagetwo.jsx
+++ src/components/Pagetwo.jsx
@@ -2,80 +2,89 @@ import React from "react";
 
 function PageTwo() {
   return (
-    <div
-      className="flex flex-col items-center min-h-screen py-10 px-5 md:px-10"
-      style={{
-        backgroundImage: "url('public/project1/Gradient bg.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+    <div>
       
-      <div className="flex flex-col md:flex-row w-full justify-between mt-10 space-y-10 md:space-y-0">
+      <div className="flex flex-col md:flex-row w-full justify-between mt-10 space-y-10 md:space-y-0 px-8">
         
+        {/* Left Content */}
         <div className="w-full md:w-1/3 text-left space-y-4 relative">
           <h1 className="text-gray-400 text-2xl">01</h1>
           <p className="text-gray-700">
             Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.
-            <br /> Faucibus in libero risus semper habitant arcu eget.
+            <br /> Faucibus in libero risus.
           </p>
 
-          <div className="mt-5">
+          <div className="mt-5 ">
             <img
-              src="public/project1/Explosion.png"
+              src="/public/project1/Explosion.png"
               alt="Explosion"
-              className=""
+              className="w-full"
             />
           </div>
-          <div>
-            
-          </div>
+
           <img
-            src="public/project1/Image bottom2.png"
+            src="/public/project1/Image bottom2.png"
             alt="Bottom Image"
-            className="mt-6"
+            className="mt-6 w-full hover:shadow-lg hover:shadow-gray-500 transition-shadow"
           />
         </div>
 
-        
-        <div className="text-center mx-auto">
-          <h1 className="text-6xl font-semibold leading-tight">
-            Create <br /> Your <br /> Design
-          </h1>
-          <p className="text-gray-600 font-thin text-lg md:text-xl mt-6 leading-relaxed">
-            Lorem ipsum dolor sit <br /> amet, consectetur <br /> adipiscing
-            elit. Faucibus <br /> in libero risus semper <br /> habitant arcu
-            eget.
-          </p>
-          <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-10 md:mt-20 w-28">
-          Explore
-        </button>
+        {/* Center Content */}
+        <div className="flex flex-col items-center justify-center min-h-screen py-10 px-5 md:px-10 relative">
+          
+          <div
+            style={{
+              backgroundImage: "url('/public/project1/Gradient bg.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              filter: "blur(8px)",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: -1, 
+            }}
+          ></div>
+
+          {/* Content div */}
+          <div className="text-center mx-auto relative z-10">
+            <h1 className="text-6xl font-semibold leading-tight">
+              Create <br /> Your <br /> Design
+            </h1>
+            <p className="text-gray-600 font-thin text-lg md:text-xl mt-6 leading-relaxed">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus in libero risus semper habitant arcu eget.
+            </p>
+            <button className="bg-black text-white py-2 px-4 rounded-full hover:bg-gray-800 transition mt-10 md:mt-20 w-28">
+              Explore
+            </button>
+          </div>
         </div>
 
-        
-        <div className="relative w-full md:w-1/3">
+        {/* Right Content */}
+        <div className="w-full md:w-1/3 relative">
           <div className="bg-blue-300 rounded-full w-48 h-48 absolute right-10 top-5"></div>
           <img
-            src="public/project1/Image top2.png"
+            src="/public/project1/Image top2.png"
             alt="Top Image"
-            className="relative"
+            className="relative w-full hover:shadow-lg hover:shadow-gray-500 transition-shadow"
           />
-        
+
           <div className="mt-5">
             <img
-              src="public\project1\Party.png"
-              alt="party"
-              className=""
+              src="/public/project1/Party.png"
+              alt="Party"
+              className="w-full"
             />
           </div>
 
-          <div className="text-left space-y-4">
-          <h1 className="text-gray-400 text-2xl">02</h1>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.{" "}
-            <br /> Faucibus in libero risus semper habitant arcu eget.
-          </p>
-        </div>
+          <div className="text-left space-y-4 mt-5">
+            <h1 className="text-gray-400 text-2xl">02</h1>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit.{" "}
+              <br /> Faucibus in libero risus.
+            </p>
+          </div>
         </div>
       </div>
     </div>
